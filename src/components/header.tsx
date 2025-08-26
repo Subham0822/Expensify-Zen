fa9474e2c6a808d970bd1cc78cccafddcaa1c5e5
Subham Rakshit <rwik0822@gmail.com>
2025-08-26 04:54:01 +0000

    make the theme system preferred too... like the options will be light, d

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,7 +21,7 @@ import { useRouter } from "next/navigation";
 export function Header() {
   const { user } = useAuth();
   const router = useRouter();
-  const { setTheme, theme } = useTheme();
+  const { setTheme } = useTheme();
 
   const handleLogout = async () => {
     await signOut();
@@ -42,16 +42,26 @@ export function Header() {
       <div className="relative ml-auto flex-1 md:grow-0">
         {/* Can be used for search in future */}
       </div>
-       <Button
-        variant="outline"
-        size="icon"
-        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        aria-label="Toggle theme"
-      >
-        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        <span className="sr-only">Toggle theme</span>
-      </Button>
+       <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
       {user && (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
